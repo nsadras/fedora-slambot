7124a25365a923eac41f0609168e105f89cdf77a
Alex Chu <alex.p.chu@gmail.com>
2014-11-25 10:23:54 +0000

    rpi video streamer

---
+++ build/catkin_generated/installspace/setup.bash
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# generated from catkin/cmake/templates/setup.bash.in
+
+CATKIN_SHELL=bash
+. "/home/pi/fedora-slambot/install/setup.sh"
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/pi/fedora-slambot/build/test_results"
+export CMAKE_PREFIX_PATH="/home/pi/fedora-slambot/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/pi/fedora-slambot/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/pi/fedora-slambot/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/home/pi/fedora-slambot/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/pi/fedora-slambot/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/pi/fedora-slambot/build"
+export PYTHONPATH="/home/pi/fedora-slambot/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROS_PACKAGE_PATH="/home/pi/fedora-slambot/src:$ROS_PACKAGE_PATH"
+export ROS_TEST_RESULTS_DIR="/home/pi/fedora-slambot/build/test_results"
