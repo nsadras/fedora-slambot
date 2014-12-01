8969e629808a068241fd052e460e728b50d4afd5
Alex Chu <alex.p.chu@gmail.com>
2014-12-01 08:01:54 +0000

    read gyro data

--- src/slam_sensors/src/encoder_pub.py
+++ src/slam_sensors/src/encoder_pub.py
@@ -7,6 +7,12 @@ MOTOR_L = 0x0a
 MOTOR_R = 0x0b
 GYRO = 0x69
 
+def to_signed_int(bits):
+    if not (bits >> 15) & 1:
+        return bits
+    else:
+        return -(((~bits) & 0xffff) + 1)
+
 def stream_encoder():
     rospy.init_node('encoder')
     pub = rospy.Publisher('encoder_data', String)
@@ -14,11 +20,17 @@ def stream_encoder():
     r = rospy.Rate(10)
 
     while not rospy.is_shutdown():
-        pub_string = i2c.read(GYRO, 0x28)
-        pub.publish(pub_string)
+        high = i2c.read(GYRO, 0x2d).strip("\n")
+	low = i2c.read(GYRO, 0x2c)[2:]
+	raw = int(high + low, 16)
+	msg = to_signed_int(raw)	
+        pub.publish(str(msg))
         r.sleep()
 
 if __name__ == '__main__':
+    i2c.write(GYRO, 0x20, 0x1f)
+    i2c.write(GYRO, 0x22, 0x08)
+    i2c.write(GYRO, 0x24, 0x80)
     try:
         stream_encoder()
     except rospy.ROSInterruptException:
