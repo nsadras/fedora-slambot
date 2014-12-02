c4853d6b908c735b9372f0c49c20a71091b85be7
nitin <nitin.sadras@gmail.com>
2014-12-02 00:20:29 -0800

    read words

--- src/slam_sensors/src/i2c.py
+++ src/slam_sensors/src/i2c.py
@@ -5,15 +5,24 @@ MOTOR_L = 0x0a
 MOTOR_R = 0x0b
 
 def read(address, register):
+    """ read a byte """
     result = check_output(["sudo", "i2cget", "-y", "1", str(address), str(register)])
     result.strip("\n")
     return result
 
+def readl(address, register):
+    """ read a word """
+    result = check_output(["sudo", "i2cget", "-y", "1", str(address), str(register), "w"])
+    result.strip("\n")
+    return result
+
 def write(address, register, value):
+    """ write a byte """
     print address, register, value
     call(["i2cset", "-y", "1", str(address), str(register), str(value)])
 
 def writel(address, register, value):
+    """ write a word """
     print address, register, value
     call(["i2cset", "-y", "1",  str(address), str(register), str(value), "w"])
 
